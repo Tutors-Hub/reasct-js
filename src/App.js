1b0ae7cd09ba8227fdcc88bf6825db2ba121eabc
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-02-26 21:55:35 +0530

    Remove unnecessary h1 element in App.js and fix typo in tailwind.config.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline bg-black text-white">
-        Hello world!
-      </h1>
+
     </div>
   );
 }
